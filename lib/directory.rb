438782df03097d2b28fac88a3448ebb76dc61574
marcoaam <marco.army@gmail.com>
2014-06-30 18:51:01 +0100

    Monday version

--- lib/directory.rb
+++ lib/directory.rb
@@ -9,3 +9,34 @@ end
 def add(student)
 	student_list << student
 end
+
+def students_to_s(students)
+	students.map {|student| "#{student[:name]}, #{student[:cohort]} cohort"}.join("\n")
+end
+
+def print_students
+	puts students_to_s(student_list)
+end
+
+def student_file(name,cohort)
+	{:name => name, :cohort => cohort.to_sym}
+end
+
+def save_students
+	File.open("students.csv","w") do|file|
+		student_list.each do |hash|
+		name = hash[:name]
+		cohort = hash[:cohort]
+		file.puts name + ',' + cohort.to_s
+		end
+	end
+end
+
+def load_students
+	File.open("students.csv","r") do |file|
+		file.readlines.each do |lines|
+			name, cohort = line.split(',')
+			add(student_file(name, cohort))
+			end		
+	end
+end
--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -1,22 +1,47 @@
 require 'directory'
 	
-	describe "student directory to" do
-		it "show a header" do
-			allow(self).to receive(:puts).with("The students of my cohort at Makers Academy")
-			show_header
-		end
-		it "has a empty students list" do
-			expect(student_list).to be_empty
-		end
-
-		it "adds a student to the students list" do
-			student = {name: "Jean", cohort: :June}
-			add(student)
-			expect(student_list).to eq [student]
-		end
-		# it "shows a list of students" do
-		# 	student = {name: "Jean", cohort: :June}
-		# 	shows.to eq "name: "
-		# end
-
+describe "student directory to" do
+	it "show a header" do
+		allow(self).to receive(:puts).with("The students of my cohort at Makers Academy")
+		show_header
 	end
+	it "has a empty students list" do
+		expect(student_list).to be_empty
+	end
+
+	it "adds a student to the students list" do
+		student = {name: "Jean", cohort: :June}
+		add(student)
+		expect(student_list).to eq [student]
+	end
+	
+	it "convert list of students to string" do
+		student = {name: "Jean", cohort: :June}
+		students = [{name: "Jean", cohort: :June}, {name: "Marco", cohort: :June}]
+		add(student)
+		expect(students_to_s(students)).to eq "Jean, June cohort\nMarco, June cohort"
+	end
+
+	it 'Prints the students to the terminal' do
+		student = {name: "Jean", cohort: :June}
+		add(student)
+
+		expect(self).to receive(:puts).with('Jean, June cohort')
+
+		print_students
+	end
+
+	it 'Creates a student hash' do
+		expect(student_file("Marco", "June")).to eq ({name: "Marco", cohort: :June})
+	end
+
+	it 'Saves a file with the list' do
+		expect(File).to receive(:open).with("students.csv","w")
+		save_students
+	end
+
+	it 'Loads from a file' do
+		expect(File).to receive(:open).with("students.csv","r")
+		load_students
+	end
+end
